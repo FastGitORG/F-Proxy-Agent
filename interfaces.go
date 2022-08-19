08a723c5444a8712b9895816c01154bb5645229b
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-08-19 03:48:50 +0100

    fix: dial dual times

--- interfaces.go
+++ interfaces.go
@@ -6,20 +6,14 @@ import (
 	"golang.org/x/net/proxy"
 )
 
-var proxyDialer proxy.Dialer = nil
-
 func GetDialer(isSocks5 bool) proxy.Dialer {
 	if !isSocks5 {
 		return &net.Dialer{}
 	}
-	if proxyDialer != nil {
-		return proxyDialer
-
-	}
-	var err error
-	proxyDialer, err = proxy.SOCKS5("tcp", cfg.SocksAddr, nil, proxy.Direct)
+	proxyDialer, err := proxy.SOCKS5("tcp", cfg.SocksAddr, nil, proxy.Direct)
 	if err != nil {
-		panic(err)
+		// FIXME: I am shit
+		return &net.Dialer{}
 	}
 	return proxyDialer
 }
